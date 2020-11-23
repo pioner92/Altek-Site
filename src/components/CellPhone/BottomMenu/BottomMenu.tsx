8197d94ff8cd77a55d40d329f875fd8c60d15721
Alex <conter10@mail.ru>
2020-11-23 11:10:48 +0100

    added classGenerate fn

--- src/components/CellPhone/BottomMenu/BottomMenu.tsx
+++ src/components/CellPhone/BottomMenu/BottomMenu.tsx
@@ -16,17 +16,21 @@ export const BottomMenu = () => {
         setSelectedButtonIndex(index)
     }
 
+    const classGenerate = (index:number) =>{
+        return `cellphone-bottom-menu__item${selectedIndex === index ? '-active':''}`
+    }
+
     return (
         <div className="cellphone-bottom-menu">
-            <div onClick={onSelect.bind(null,historyIndex)} className={`cellphone-bottom-menu__item ${selectedIndex === historyIndex && 'active'}`}>
+            <div onClick={onSelect.bind(null,historyIndex)} className={classGenerate(historyIndex)}>
                 <img src={phone_history} alt=""/>
                 <span>History</span>
             </div>
-            <div onClick={onSelect.bind(null,faxIndex)} className={`cellphone-bottom-menu__item ${selectedIndex === faxIndex && 'active'}`}>
+            <div onClick={onSelect.bind(null,faxIndex)} className={classGenerate(faxIndex)}>
                 <img src={phone_fax} alt=""/>
                 <span>Fax</span>
             </div>
-            <div onClick={onSelect.bind(null,chatIndex)} className={`cellphone-bottom-menu__item ${selectedIndex === chatIndex && 'active'}`}>
+            <div onClick={onSelect.bind(null,chatIndex)} className={classGenerate(chatIndex)}>
                 <img src={phone_chat} alt=""/>
                 <span>Chat</span>
             </div>
